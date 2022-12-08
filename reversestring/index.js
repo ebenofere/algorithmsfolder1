ed25f2400d892f7380fc7c70835f601ddf332cff
Eben Ofere <eofere@gmail.com>
2022-12-08 16:32:14 -0500

    reverse string

--- reversestring/index.js
+++ reversestring/index.js
@@ -6,6 +6,22 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//   // Step 1: Use the split() method to return a new array
+//   var splitString = str.split("");
+
+//   // Step 2: Use the reverse() method to reverse the newly created array
+//   var reverseArray = splitString.reverse();
+
+//   //   Step 3: Use the join() method to join all elements of the array into a string
+//   var joinArray = reverseArray.join("");
+
+//   //   Step 4: Return the reversed string
+//   return joinArray;
+// }
+
+function reverse(str) {
+  return (finalArray = str.split("").reverse().join(""));
+}
 
 module.exports = reverse;
